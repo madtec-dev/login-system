f56f029964a7d0a0ab514d1b981f4821fe4ad8bd
Giorgix <jackosinapsis@gmail.com>
2014-12-18 05:46:42 +0100

    added simple passport-local AUTH system

--- config/passport.js
+++ config/passport.js
@@ -42,4 +42,21 @@ module.exports = function(passport) {
     });
     });
   }));
+  passport.use('local-login', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  function(req, email, password, done){
+    User.findOne({'local.email' : email }, function(err, user) {
+      if(err) { return done(err); }
+      if(!user) {
+        return done(null, false, req.flash('loginMessage', 'Wrong email or password.'));
+      }
+      if(!user.validPassword(password)) {
+        return done(null, false, req.flash('loginMessage', 'Wrong email or password.'));
+      }
+      return done(null, user);
+    });
+  }));
 };
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,12 @@ module.exports = function(app, passport) {
   app.get('/login', function(req, res) {
     res.render('login', {message: req.flash('loginMessage') });
   });
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile',
+    failureRedirect: '/login',
+
+    failureFlash: true
+  }))
   
   // SIGNUP =======================================
   app.get('/signup', function(req, res) {
@@ -20,4 +26,21 @@ module.exports = function(app, passport) {
 
     failureFlash: true
   }));
+  // PROFILE =======================================
+  app.get('/profile', isLoggedIn, function(req, res) {
+    console.log(req.user);
+    res.render('profile', {user : req.user})
+  })
+  // LOGOUT ========================
+  app.get('/logout', function(req, res) {
+    req.logout;
+    res.redirect('/');
+  })
+
+  function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/');
+  }
 }
