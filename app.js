5dc5f2801cecaf93040c2f71db02bd4c8633b971
Giorgix <jackosinapsis@gmail.com>
2014-12-18 05:01:00 +0100

    added simple passport-local sign-up system

--- app.js
+++ app.js
@@ -1,31 +1,54 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+var path = require('path');
+var flash = require('connect-flash');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
 
+var configDB = require('./config/database.js');
+
+// DB ===================================
+mongoose.connect(configDB.url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+  console.log('Connected to DB');
+})
+
+
+// CONFIG =============================================================
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+require('./config/passport')(passport);
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'AFDADGF2394021930546=--021945!@#%#$^SADFSD',
+  resave: false,
+  saveUninitialized: true
 
-app.use('/', routes);
-app.use('/users', users);
+}));
 
-// catch 404 and forward to error handler
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// ROUTES ========================================
+require('./routes/index.js')(app, passport);
+// USER schema ==========================
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(app, passport) {
+  
+  // GET HOME ===================================
+  app.get('/', function(req, res) {
+    res.render('index');
+  });
+  // LOGIN ======================================
+  app.get('/login', function(req, res) {
+    res.render('login', {message: req.flash('loginMessage') });
+  });
+  
+  // SIGNUP =======================================
+  app.get('/signup', function(req, res) {
+    res.render('signup', {message: req.flash('signupMessage') });
+  });
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
 
-module.exports = router;
+    failureFlash: true
+  }));
+}
